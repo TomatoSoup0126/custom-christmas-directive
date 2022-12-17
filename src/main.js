43b02b4fd29215c4ac30428eaa1709a5341a2340
TomatoSoup <jonathantang0126@gmail.com>
2022-12-18 00:05:32 +0800

    feat: 🎸 add directives

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import './base.css'
 
 const app = createApp(App)
 
-app.directive('christmas', (/* todo*/) => {
-  /* todo*/
+app.directive('christmas', {
+  beforeMount(el, binding) {
+    el.classList.add('christmas-text')
+    if (binding.arg) {
+      el.classList.add(binding.arg)
+    } else {
+      el.classList.add('red-green')
+    }
+
+    if (binding.value) {
+      el.style.animationDuration = `${binding.value}s`
+    }
+  }
 })
 
 app.mount('#app')
